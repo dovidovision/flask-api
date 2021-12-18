fc60b27165c9041a84117085faeacff66445f0d5
moon <larcane@naver.com>
2021-12-18 09:57:24 +0000

    modify app.py structure

--- api.py
+++ api.py
@@ -3,13 +3,119 @@ from flask_cors import CORS
 from io import BytesIO
 from PIL import Image
 from io import BytesIO
+import albumentations as A
+import albumentations.pytorch as AP
 import base64
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
+import clip
+import torch
+from model import MMSegModel, get_clip,KoGPT
+# from transformers import AutoTokenizer, AutoModelForCausalLM 
+# import os
+# import kss
+# os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 app = Flask (__name__)
 CORS(app)
+
+## model load
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+# device='cpu'
+
+seg_model = MMSegModel(
+    cfg='/opt/ml/segmentation_module/mmsegmentation/work_dirs/swinT_segformer_add_pascal/swinT_segformer.py',
+    checkpoint='/opt/ml/segmentation_module/mmsegmentation/work_dirs/swinT_segformer_add_pascal/best_mIoU_epoch_11.pth',
+    device=device)
+
+gpt_model = KoGPT(pretrained='larcane/kogpt2-cat-diary',device=device)
+
+clip_model, _ = get_clip(device)
+_ = clip_model.eval()
+
+preprocess = A.Compose([
+    A.SmallestMaxSize(clip_model.visual.input_resolution),
+    A.CenterCrop(clip_model.visual.input_resolution,clip_model.visual.input_resolution),
+    A.Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711)),
+    AP.ToTensorV2()
+])
+
+emotions=[
+    'a picture of hipster cat',
+    'a picture of sad cat',
+    'a picture of cute cat',
+    'a picture of angry cat',
+    'a picture of happy cat',
+    'a picture of surprised cat'
+]
+actions=[
+    'a picture of cat playing',
+    'a picture of cat eating',
+    'a picture of sleeping cat',
+    'a picture of cat wearing a mask',
+    'a picture of running cat',
+    'a picture of jumping cat',
+]
+
+actions = [
+    'a picture of cat lying down',
+    'a picture of cat lying on side',
+    'a picture of grooming cat',
+    'a picture of playing cat',
+    'a picture of cat punching',
+    'a picture of cat eating',
+    # 'hugged cat',
+    # 'box cat',
+    # 'liquid cat',
+    # 'upside down cat',
+]
+
+emotions = [
+    'a picture of sleeping cat',
+    'a picture of sleepy cat', # 누워서 자고 있지 않은 것 (하품, 앉아서 자는 고양이 등)
+    'a picture of happy cat',
+    'a picture of comfortable cat',
+    'a picture of curious cat',
+    'a picture of perplexed cat',
+    'a picture of scared cat',
+    'a picture of sad cat',
+    'a picture of cloudy cat',
+    'a picture of angry cat',
+]
+
+labels = [  'a picture of cat playing',
+            'a picture of cat eating',
+            'a picture of hipster cat','a picture of sad cat',
+            'a picture of cute cat','a picture of angry cat',
+            'a picture of cat wearing a mask','a picture of sleeping cat',
+            'a picture of happy cat','a picture of running cat',
+            'a picture of surprised cat'
+            ]
+
+labels_eng2kor = {
+    'a picture of cat lying down':"엎드려있는 고양이",
+    'a picture of cat lying on side':"옆으로 누워있는 고양이",
+    'a picture of grooming cat' :"그루밍하는 고양이",
+    'a picture of playing cat' :"놀고있는 고양이",
+    'a picture of cat punching' :"펀치를 하는 고양이",
+    'a picture of cat eating' :"밥을 먹는 고양이",
+    'a picture of sleeping cat' :"자고 있는 고양이",
+    'a picture of sleepy cat':"졸린 고양이", 
+    'a picture of happy cat' :"행복한 고양이",
+    'a picture of comfortable cat' :"편안한 고양이",
+    'a picture of curious cat':"궁금해하는 고양이",
+    'a picture of perplexed cat':"당황한 고양이",
+    'a picture of scared cat':"무서워하는 고양이",
+    'a picture of sad cat':"슬픈 고양이",
+    'a picture of cloudy cat':"언짢은 고양이",
+    'a picture of angry cat':"화난 고양이",
+}
+
+
+action_tokens = clip.tokenize(actions).to(device)
+emotion_tokens = clip.tokenize(emotions).to(device)
+
 
 @app.route('/hello')
 def hello():
@@ -21,16 +127,76 @@ def hello():
 @app.route('/image', methods = ['POST'])
 def image():
     data = request.get_json()
-    img = Image.open(BytesIO(base64.b64decode(data['image'].split(',')[-1])))
-    img = img.convert('L')
+    img = Image.open(BytesIO(base64.b64decode(data['image'].split(',')[-1]))).convert('RGB')
+    img = np.array(img)
 
-    plt.imshow(img)
+
+    _,masked_img = seg_model(img)
+    masked_img = masked_img.astype(np.uint8)
+    # processed_img = get_segmap(seg_model,img,seg_preprocess)
+    # plt.imshow(processed_img)
+    # plt.savefig('segmap.png')
+
+    if isinstance(preprocess,A.Compose):
+        processed_img = preprocess(image=img)['image'].unsqueeze(0).to(device)
+        processed_masked_img = preprocess(image=masked_img)['image'].unsqueeze(0).to(device)
+    else:
+        processed_img = preprocess(img).unsqueeze(0).to(device)
+        processed_masked_img = preprocess(masked_img).unsqueeze(0).to(device)
+
+        
+    # 1. masked_img와 emotion token/ action token
+    # 2. emotion token이 큰 순서대로 action token에 곱함
+    # 3. img로 action token
+    # 4. action token끼리 곱
+    # 5. 최대 token 출력
+
+    with torch.no_grad():
+        image_features = clip_model.encode_image(torch.cat([processed_masked_img,processed_img],dim=0))
+        text_features = clip_model.encode_text(torch.cat([action_tokens,emotion_tokens],dim=0))
+
+        # normalized features
+        image_features = image_features / image_features.norm(dim=-1, keepdim=True)
+        text_features = text_features / text_features.norm(dim=-1, keepdim=True)
+
+        masked_img_logit,origin_img_logit = image_features@text_features.T
+
+        masked_action_logit = masked_img_logit.squeeze()[:len(actions)]
+        masked_emotion_logit = masked_img_logit.squeeze()[len(actions):]
+        origin_action_logit = origin_img_logit.squeeze()[:len(actions)]
+        origin_emotion_logit = origin_img_logit.squeeze()[len(actions):]
+
+        masked_pred_emotion = emotions[masked_emotion_logit.argmax()]
+        masked_pred_action = actions[masked_action_logit.argmax()]
+        origin_pred_emotion = emotions[origin_emotion_logit.argmax()]
+        origin_pred_action = actions[origin_action_logit.argmax()]
+        com_pred_action =  actions[(origin_action_logit*masked_action_logit).argmax()]
+
+        kor_action = labels_eng2kor[com_pred_action]
+        kor_emotion = labels_eng2kor[masked_pred_emotion]
+
+        action_text = gpt_model(kor_action)
+        emotion_text = gpt_model(kor_emotion)
+        # f"action:[{com_pred_action}]\nemotion:[{masked_pred_emotion}]"
+
+        # image_logit,_ = clip_model.forward(processed_img,token_labels)
+        # probs = image_logit.softmax(dim=-1).cpu().numpy()
+        # pred = probs.argmax()
+        # text = labels[pred]
+
+
+        # prompt = label2prompt[text]
+        # tokens = gpt_tokenizer.encode(prompt, return_tensors='pt').to(device='cuda', non_blocking=True)
+        # gen_tokens = gpt_model.generate(tokens, do_sample=True, temperature=0.8, max_length=128)
+        # generated = gpt_tokenizer.batch_decode(gen_tokens)[0]
+            
 
     buffered = BytesIO()
+    img = Image.fromarray(img)
     img.save(buffered, format="JPEG")
     img_str = base64.b64encode(buffered.getvalue())
 
-    return {'image': str(img_str), 'text':'test text'}
+    return {'image': str(img_str), 'text':f"action:[{kor_action}]{action_text}\n\nemotion:[{kor_emotion}]{emotion_text}"}
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=5000)
+    app.run(host='0.0.0.0',port=6006)
---
+++ model.py
@@ -0,0 +1,106 @@
+import torch
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import clip
+
+import re
+from transformers import PreTrainedTokenizerFast,GPT2LMHeadModel
+class KoGPT:
+    def __init__(self,pretrained='larcane/kogpt2-cat-diary',device='cpu'):
+        self.model = GPT2LMHeadModel.from_pretrained(pretrained).to(device)
+        self.tokenizer = PreTrainedTokenizerFast.from_pretrained(pretrained,
+                    bos_token='<s>', eos_token='</s>', unk_token='<unk>',
+                    pad_token='<pad>', mask_token='<mask>')
+        self.device=device
+
+    def __call__(self,label,max_length=128,top_k=200,top_p=0.95):
+        token,head = self.preprocess(label)
+        input_ids = self.tokenizer.encode(token,return_tensors='pt').to(self.device)
+        generated = self.model.generate(
+            input_ids,
+            do_sample=True,
+            num_return_sequences=1,
+            max_length=max_length, 
+            top_k=top_k, 
+            top_p=top_p,
+            # temperature=0.9,
+            # eos_token_id=tokenizer.eos_token_id,
+            early_stopping=True,
+            bad_words_ids=[[self.tokenizer.unk_token_id]]
+        ).tolist()
+        text = self.tokenizer.decode(generated[0],skip_special_tokens=True)
+        return self.postprocess(text,head)
+
+    def preprocess(self,label):
+        head = label.strip()+"의 일기 ::"
+        return f"<s>{head}</s><s>",head
+
+    def postprocess(self,text,head):
+        return re.sub(head,'',text).strip()
+
+
+
+import mmcv
+from mmcv.runner import load_checkpoint
+from mmseg.models import build_segmentor
+from mmcv.parallel import MMDataParallel
+from mmseg.datasets.pipelines import Compose
+import cv2
+
+class MMSegModel:
+    def __init__(self,cfg,checkpoint,device):
+        cfg = mmcv.Config.fromfile(cfg)
+        cfg.model.pretrained = None
+        cfg.model.train_cfg = None
+        cfg.data.test.test_mode = True
+
+        segmentor = build_segmentor(
+            cfg.model, train_cfg=None, test_cfg=cfg.get('test_cfg'))
+        # segmentor.load_state_dict(a['state_dict'])
+        load_checkpoint(segmentor,checkpoint, map_location='cpu')
+        segmentor.eval()
+        self.segmentor = MMDataParallel(segmentor, device_ids=[0]).to(device)
+        self.transform = Compose([cfg.data.test['pipeline'][1]])
+        self.device=device
+
+    def __call__(self,image):
+        masked_image = image.copy()
+        mask = self.segmenting(image)  # x shape : H,W,3
+        try:
+            x1 = np.where(mask.max(axis=0)==1)[0].min()
+        except: x1=0
+        try:
+            y1 = np.where(mask.max(axis=1)==1)[0].min()
+        except: y1=0
+        try:
+            x2 = np.where(mask.max(axis=0)==1)[0].max()
+        except: x2=mask.shape[1]
+        try:
+            y2 = np.where(mask.max(axis=1)==1)[0].max()
+        except: y2=mask.shape[2]
+
+        masked_image[mask==0]=255 # HxWx3
+        masked_image = masked_image[y1:y2,x1:x2,:]
+        return (image,masked_image)
+
+        
+    def segmenting(self,x):
+        data = self.preprocess(x)
+        data = self.transform(data)
+        data = self.postprocess(data)
+        return self.segmentor.forward(return_loss=False,**data)[0]
+
+    def preprocess(self,data):
+        return dict(img=data,filename='',ori_filename='',ori_shape=data.shape)
+
+    def postprocess(self,data):
+        data['img_metas'][0]._data=[[data['img_metas'][0]._data]]
+        data['img'][0] = data['img'][0].unsqueeze(0)
+        return data
+
+
+
+def get_clip(device):
+    clip_model, preprocess = clip.load("ViT-B/32", device=device)
+    return clip_model,preprocess
